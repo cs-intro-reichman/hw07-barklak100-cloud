24b870111141a9821c4a6995c4d40f030e5c68c3
barklak100-cloud <barklak100@gmail.com>
2025-12-31 21:27:53 +0200

    done

--- Binomial.java
+++ Binomial.java
@@ -1,16 +1,11 @@
 public class Binomial { 
-    
-    public static void main(String[] args) {
-        int n = 50;
-        int k = 20;
-        System.out.println("Result for (" + n + "," + k + "): " + binomial(n, k));
-    }
-    
-    public static long binomial(int n, int k) {
-        if (k > n) return 0;
-        if (k < 0) return 0; 
-        if (k == 0 || n == k) return 1;
 
+    public static void main(String[] args) {
+    }
+
+    public static long binomial(int n, int k) {
+        if (k > n || k < 0) return 0;
+        if (k == 0 || n == k) return 1;
         long[][] memo = new long[n + 1][k + 1];
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= k; j++) {
@@ -23,30 +18,19 @@ public class Binomial {
     private static long calculateBinomial(int n, int k, long[][] memo) {
         if (k > n || k < 0) return 0;
         if (k == 0 || n == k) return 1;
-
-        if (memo[n][k] != -1) {
-            return memo[n][k];
-        }
-
+        if (memo[n][k] != -1) return memo[n][k];
         memo[n][k] = calculateBinomial(n - 1, k, memo) + calculateBinomial(n - 1, k - 1, memo);
         return memo[n][k];
     }
 
-    public static long binomial1(int n, int k) {
-        return binomial(n, k);
-    }
-}
-
-	public static boolean isSorted(int[] arr) {
-    	if (arr == null || arr.length <= 1) {
+    public static boolean isSorted(int[] arr) {
+        if (arr == null || arr.length <= 1) return true;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
         return true;
     }
 
-   		 for (int i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false; 
-        }
-    }
-    
-    return true;
-}
+  }
