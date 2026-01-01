6ed40f376adbeb569121b4f4fd4868e4fd6cd2be
barklak100-cloud <barklak100@gmail.com>
2026-01-01 12:33:49 +0200

    done

--- Sierpinski.java
+++ Sierpinski.java
@@ -1,40 +1,40 @@
-/** Draws ths Sierpinski Triangle fractal. */
-public class Sierpinski {
-	
-	public static void main(String[] args) {
-		sierpinski(Integer.parseInt(args[0]));
-	}
-	
-	// Draws a Sierpinski triangle of depth n on the standard canvass.
-	public static void sierpinski (int n) {
-		double x1 = 0.0, y1 = 0.0;           
-    	double x2 = 1.0, y2 = 0.0;            
-   		double x3 = 0.5, y3 = Math.sqrt(3)/2;
-		
-		StdDraw.line(x1, y1, x2, y2);
-    	StdDraw.line(x2, y2, x3, y3);
-    	StdDraw.line(x3, y3, x1, y1);
 
-		sierpinski(n, x1, x2, x3, y1, y2, y3);
-	}
-	// Does the actual drawing, recursively.
-	private static void sierpinski(int n, double x1, double x2, double x3,
-		                                 double y1, double y2, double y3) {
-if (n == 0) return; 
+	public class Sierpinski {
 
-    double x12 = (x1 + x2) / 2.0;
-    double y12 = (y1 + y2) / 2.0;
-    double x23 = (x2 + x3) / 2.0;
-    double y23 = (y2 + y3) / 2.0;
-    double x31 = (x3 + x1) / 2.0;
-    double y31 = (y3 + y1) / 2.0;
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        sierpinski(n);
+    }
 
-    StdDraw.line(x12, y12, x23, y23);
-    StdDraw.line(x23, y23, x31, y31);
-    StdDraw.line(x31, y31, x12, y12);
+    public static void sierpinski(int n) {
+        double x1 = 0.0, y1 = 0.0;
+        double x2 = 1.0, y2 = 0.0;
+        double x3 = 0.5, y3 = Math.sqrt(3) / 2.0; 
 
-    sierpinski(n - 1, x1, x12, x31, y1, y12, y31);
-    sierpinski(n - 1, x12, x2, x23, y12, y2, y23);
-    sierpinski(n - 1, x31, x23, x3, y31, y23, y3);
-}	}
+        StdDraw.line(x1, y1, x2, y2);
+        StdDraw.line(x2, y2, x3, y3);
+        StdDraw.line(x3, y3, x1, y1);
 
+        sierpinski(n, x1, x2, x3, y1, y2, y3);
+    }
+
+    private static void sierpinski(int n, double x1, double x2, double x3,
+                                   double y1, double y2, double y3) {
+        if (n == 0) return; 
+
+        double x12 = (x1 + x2) / 2.0;
+        double y12 = (y1 + y2) / 2.0;
+        double x23 = (x2 + x3) / 2.0;
+        double y23 = (y2 + y3) / 2.0;
+        double x31 = (x3 + x1) / 2.0;
+        double y31 = (y3 + y1) / 2.0;
+
+        StdDraw.line(x12, y12, x23, y23);
+        StdDraw.line(x23, y23, x31, y31);
+        StdDraw.line(x31, y31, x12, y12);
+
+        sierpinski(n - 1, x1, x12, x31, y1, y12, y31);   
+        sierpinski(n - 1, x12, x2, x23, y12, y2, y23);  
+        sierpinski(n - 1, x31, x23, x3, y31, y23, y3);   
+    }
+}
