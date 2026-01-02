0afe55d33e15238e54874b6d802d6fcff23c1068
barklak100-cloud <barklak100@gmail.com>
2026-01-02 15:01:35 +0200

    done

--- Binomial.java
+++ Binomial.java
@@ -29,14 +29,18 @@ public class Binomial {
         return memo[n][k];
     }
 
-     public static boolean isSorted(int[] arr) {
+    public static boolean isSorted(int[] arr) {
+    if (arr.length <= 1) {
+        return true;
+    }
+
     for (int i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
+        if (arr[i] > arr[i + 1]) { 
             return false; 
         }
     }
-    return true;
 
+    return true; 
 }
     public static String intToBin(int n) {
         if (n == 0) return "0";
